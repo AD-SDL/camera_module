e1f93090c1a7bcf0a318e15d2cd5a3dfc2b08c00
Ryan Lewis <accounts+github@ryandlewis.dev>
2024-06-17 12:36:03 -0500

    Cleanup and get tests/checks passing

--- src/camera_rest_node.py
+++ src/camera_rest_node.py
@@ -2,54 +2,60 @@
 REST-based node that interfaces with WEI and provides a USB camera interface
 """
 
-from contextlib import asynccontextmanager
 from pathlib import Path
-from typing_extensions import Annotated
 
 import cv2
 import numpy as np
+from fastapi.datastructures import State
+from typing_extensions import Annotated
+from wei.modules.rest_module import RESTModule
 from wei.types.step_types import (
+    ActionRequest,
     StepFileResponse,
     StepResponse,
     StepStatus,
-    ActionRequest
 )
 from wei.utils import extract_version
-from wei.modules.rest_module import RESTModule
-from fastapi.datastructures import State
 
 rest_module = RESTModule(
-        name="camera_node",
-        version=extract_version(Path(__file__).parent.parent / "pyproject.toml"),
-        description="An example REST camera  implementation",
-        model="camera"
-        
+    name="camera_node",
+    version=extract_version(Path(__file__).parent.parent / "pyproject.toml"),
+    description="An example REST camera  implementation",
+    model="camera",
 )
-rest_module.arg_parser.add_argument("--camera_address", type=str, help="the camera address", default="/dev/video0")
+rest_module.arg_parser.add_argument(
+    "--camera_address", type=str, help="the camera address", default="/dev/video0"
+)
 
 
-@rest_module.action(name="take_picture", description="An action that atkes and returns a picture")
-def take_picture(state: State, 
-                 action: ActionRequest,
-                 file_name: Annotated[str, "Name of the file to save"] = "image.jpg") -> StepResponse:
-        """Function to take a picture"""
-        image_name = file_name
-        image_path = Path("~/.wei/temp").expanduser() / image_name
-        image_path.parent.mkdir(parents=True, exist_ok=True)
-        try:
-            camera = cv2.VideoCapture(state.camera_address)
-            _, frame = camera.read()
-            cv2.imwrite(str(image_path), frame)
-            camera.release()
-        except Exception:
-            print("Camera unavailable, returning empty image")
-            blank_image = np.zeros(shape=[512, 512, 3], dtype=np.uint8)
-            cv2.imwrite(str(image_path), blank_image)
+@rest_module.action(
+    name="take_picture", description="An action that atkes and returns a picture"
+)
+def take_picture(
+    state: State,
+    action: ActionRequest,
+    file_name: Annotated[str, "Name of the file to save"] = "image.jpg",
+) -> StepResponse:
+    """Function to take a picture"""
+    image_name = file_name
+    image_path = Path("~/.wei/temp").expanduser() / image_name
+    image_path.parent.mkdir(parents=True, exist_ok=True)
+    try:
+        camera = cv2.VideoCapture(state.camera_address)
+        _, frame = camera.read()
+        cv2.imwrite(str(image_path), frame)
+        camera.release()
+    except Exception:
+        print("Camera unavailable, returning empty image")
+        blank_image = np.zeros(shape=[512, 512, 3], dtype=np.uint8)
+        cv2.imwrite(str(image_path), blank_image)
 
-        return StepFileResponse(
-            action_response=StepStatus.SUCCEEDED,
-            path=image_path,
-            action_log="",
-        )
+    return StepFileResponse(
+        action_response=StepStatus.SUCCEEDED,
+        path=image_path,
+        action_log="",
+    )
 
-rest_module.start()
+
+if __name__ == "__main__":
+    rest_module.start()
